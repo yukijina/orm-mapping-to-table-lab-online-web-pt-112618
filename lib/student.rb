47f3c93d34cddd58ca78b8107e52e824af94335c
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-12 15:14:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,7 @@ class Student
     SQL
     DB[:conn].execute(sql, self.name, self.grade )  
     
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students").flatten
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students").flatten[0]
   end 
   
   
